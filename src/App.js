5c1407a2979e51100ce252ba1cf02e10b5a64ca1
wangkaiwd <1162486176@qq.com>
2018-12-10 22:37:42 +0800

    feat: react-create-app默认支持css-modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import styles from './App.module'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.app}>
         App
       </div>
     );
